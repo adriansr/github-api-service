be9ab2e56081358083eebbf51ae40f30686fb615
Adrian Serrano <adrisr83@gmail.com>
2017-07-24 23:03:59 +0200

    Cleanup of service main

--- cmd/service/service.go
+++ cmd/service/service.go
@@ -1,10 +1,8 @@
+// Package main contains the main implementation of the `service` application
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"strconv"
 
 	"github.com/adriansr/github-api-service/config"
 	"github.com/adriansr/github-api-service/githubapi"
@@ -14,34 +12,6 @@ import (
 const (
 	configFilePath = "config.json"
 )
-
-func main_Demo1() {
-	location := os.Args[1]
-	count, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		log.Fatal(err)
-	}
-	config, err := config.LoadFile(configFilePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	client, err := githubapi.NewClient(
-		config.Credentials.Username,
-		config.Credentials.Password,
-		config.Client.ApiUrl,
-		config.Client.RequestTimeout.Duration)
-	if err != nil {
-		log.Fatal(err)
-	}
-	result, err := client.GetTopContributors(location, count)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Got %d results:\n", len(result))
-	for idx, elem := range result {
-		fmt.Printf("%d: %s (id:%d)\n", idx, elem.Username, elem.ID)
-	}
-}
 
 func main() {
 	config, err := config.LoadFile(configFilePath)
