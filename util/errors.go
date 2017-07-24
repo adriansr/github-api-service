69a52c991acd07c271f5b735399d28f3dafbb483
Adrian Serrano <adrisr83@gmail.com>
2017-07-24 19:09:02 +0200

    documentation for util package

--- util/errors.go
+++ util/errors.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 )
 
+// WrapError decorates an existing error (possibly returned by a 3rd party
+// library) with the given message, for context
 func WrapError(message string, cause error) error {
 	return fmt.Errorf("%s [caused by: %s]", message, cause.Error())
 }
 
+// NewError creates an error that prints the given string
 func NewError(message string) error {
 	return errors.New(message)
 }
--- util/func.go
+++ util/func.go
@@ -1,5 +1,8 @@
+// Package util provides small utilities used through the rest
+// of the project
 package util
 
+// Min returns the minimum of two ints
 func Min(a, b int) int {
 	if a < b {
 		return a
@@ -7,6 +10,7 @@ func Min(a, b int) int {
 	return b
 }
 
+// Max returns the maximum of two ints
 func Max(a, b int) int {
 	if a > b {
 		return a
